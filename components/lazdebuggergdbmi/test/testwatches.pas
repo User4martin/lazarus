51cbc86a2f26d716570cc00ed5345e7fd2e7c75c
Martin <laz.git@mfriebe.de>
2021-11-23 18:26:15 +0100

    IdeDebugger: New Package IdeDebugger. Move some classes from DebuggerIntf

--- components/lazdebuggergdbmi/test/testwatches.pas
+++ components/lazdebuggergdbmi/test/testwatches.pas
@@ -8,7 +8,7 @@ uses
   Classes, SysUtils, fpcunit, testutils, testregistry, RegExpr,
   DbgIntfBaseTypes, DbgIntfDebuggerBase, TestBase, GDBMIDebugger, TestDbgConfig,
   TestDbgControl, TTestDbgExecuteables, TestDbgTestSuites, LCLProc,
-  TestWatchUtils;
+  TestWatchUtils, DbgGuiBaseDebugger;
 
 const
   BREAK_LINE_FOOFUNC_NEST  = 206;
