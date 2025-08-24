c7cdb6fb40989cb8e3aebe9f5b763a841ea80b46
Martin <laz.git@mfriebe.de>
2025-08-24 14:52:14 +0200

    SynEdit: Markup HighlightAll, change scanmode for caret to ExtendASync

--- components/synedit/syneditmarkuphighall.pp
+++ components/synedit/syneditmarkuphighall.pp
@@ -3719,6 +3719,7 @@ begin
   FStateChanged := False;
   FValidateNeeded := False;
   HideSingleMatch := True;
+  FScanMode := smsmDirectExtendASync;
   FFullWord := False;
   FWaitTime := 1500;
   FTrim := True;
