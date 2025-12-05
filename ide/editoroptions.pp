fb669684c85160ab328695073fd2828d55d98d13
Martin <laz.git@mfriebe.de>
2025-12-05 23:24:17 +0100

    IDE: Color-Options: Search MarkupManager for colors that can be clicked.

--- ide/editoroptions.pp
+++ ide/editoroptions.pp
@@ -7254,6 +7254,7 @@ begin
         TSynHighlighterLazCustomPasAttribute(aDest).CustomWords.Assign(CustomWords);
         TSynHighlighterLazCustomPasAttribute(aDest).CustomWordTokenKind := CustomWordTokenKind;
       end;
+      if aDest.StoredName = '' then aDest.StoredName := Src.StoredName;
     end;
 
     if hafPrior in Src.AttrFeatures then begin
